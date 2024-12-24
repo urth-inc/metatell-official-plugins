dc045d28f58a6058a3e61bf417a9f0cb0b3cc92f
kira924age <kira924age@gmail.com>
2024-12-24 17:34:17 +0900

    Remove dist from repository

--- password-collection-modal/dist/@mf-types.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'REMOTE_ALIAS_IDENTIFIER/CustomOverlay';
-    type PackageType<T> = T extends 'REMOTE_ALIAS_IDENTIFIER/CustomOverlay' ? typeof import('REMOTE_ALIAS_IDENTIFIER/CustomOverlay') :any;
